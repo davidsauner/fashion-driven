aa8d865522d7585188a8a210c16f8400fa289869
david <davidsauner@gmail.com>
2022-07-05 18:44:39 -0300

    laytou e seleção pronto

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-alert("ihulllllllllll")
+let modelo;
+let gola;
+let tecido;
+
+function selecionacamisa(borda , nome){
+    let camisa = document.querySelector('.selecionado')
+    modelo = nome
+    if(camisa !== null){
+        camisa.classList.remove('selecionado')
+    }
+    
+    borda.classList.add('selecionado')
+}
+function selecionagola(bgola , nome){
+    let camisagola = document.querySelector('.golinha .selecionado')
+    gola = nome
+    if(camisagola !== null){
+        camisagola.classList.remove('selecionado')
+        
+    }
+    
+    bgola.classList.add('selecionado')
+}
+
+function selecionatecido(tecidin , nome){
+    let tecidao = document.querySelector('.tecido .selecionado')
+    tecido = nome
+
+    if(tecidao !== null){
+        tecidao.classList.remove('selecionado')
+        
+    }
+    
+    tecidin.classList.add('selecionado')
+}
